a69f2edd7c8021215c7be6be013eda1d6813f678
Akraja712 <ashokkumar7hitter@gmail.com>
2024-09-02 17:21:14 +0530

    OrdersController.php

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -36,7 +36,7 @@ class OrdersController extends Controller
                     $product = Products::find($order->product_id);
                     $price = $order->price;
                     $delivery_charges = $order->delivery_charges;
-                    $total_price = $order->total_price;
+                    $total_price = $order->price;
                     $address1 = $address->door_no . ' ' . $address->street_name;
                     $payment_mode = $order->payment_mode;
     
