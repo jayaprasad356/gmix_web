56a8978012bf10fede432c1a7f9e3ffa0f7a2f90
Akraja712 <ashokkumar7hitter@gmail.com>
2024-09-03 10:29:11 +0530

    home.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Points;
 use App\Models\Users;
-use App\Models\Trips;
+use App\Models\Orders;
 use App\Models\Verifications;
 use App\Models\Transactions;
 use Illuminate\Http\Request;
@@ -29,14 +29,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-       
+        $startOfDay = Carbon::today(); // Start of the day (00:00:00)
+        $endOfDay = Carbon::today()->setTime(23, 59, 59); // End of the day (23:59:59)
+        $today = Carbon::today()->format('Y-m-d');
+
+      
+        $today_customers = Users::whereDate('created_at', $today)
+        ->count();
+
+        $today_orders = Orders::whereDate('created_at', $today)
+        ->count();
+  
         
         // Optional: Count of pending profiles and cover images
         // $pending_profile_count = Users::where('profile_verified', 0)->whereNotNull('profile')->count();
         // $pending_cover_image_count = Users::where('profile_verified', 0)->whereNotNull('cover_img')->count();
         
         return view('home', [
-            
+            'today_customers' => $today_customers,
+            'today_orders' => $today_orders,
+           
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,40 @@
 @section('content-header', 'Dashboard')
 
 @section('content')
-   
+    <div class="container-fluid">
+        <div class="row">
+            <!-- small box -->
+            <div class="col-lg-4 col-6">
+              <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>{{ $today_customers}}</h3>
+                        <p>Today Customers</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-users"></i> <!-- Example of a different icon -->
+                    </div>
+                    <a href="{{ route('users.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-6">
+            <div class="small-box bg-purple">
+                    <div class="inner">
+                        <h3>{{ $today_orders }}</h3>
+                        <p>Today Orders</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-shopping-cart"></i> <!-- Example of a different icon -->
+                    </div>
+                    <a href="{{ route('orders.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+          
+
+
+            <!-- ./col -->
+        </div>
+        <!-- /.row -->
+    </div>
 @endsection
